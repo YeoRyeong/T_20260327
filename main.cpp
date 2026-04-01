68098ea776cab9d61715e928a1ea92cfcbe112bb
Jun <tjrwns0012@naver.com>
2026-04-01 10:27:58 +0900

    winapi

--- main.cpp
+++ main.cpp
@@ -2,16 +2,35 @@
 #include "Engine.h"
 #include "World.h"
 
+#include "SDL.h"
+//#include "SDL_main.h"
+// 사용할 라이브러리 복사
+// include 경로 추가
+// lib 경로 추가
+// libaray 파일 등록
+
+// 사용할 라이브러리 파일 추가
+#pragma comment(lib, "SDL2")
+#pragma comment(lib, "SDL2main")
+
 using namespace std;
 
 
-int main()
+int SDL_main(int argc, char* argv[])
 {
-	GEngine->GetWorld()->Load("Map.txt");
-
-	GEngine->Run();
-
-	delete GEngine;
 
 	return 0;
 }
+
+
+// 
+// int main()
+// {
+// 	GEngine->GetWorld()->Load("Map.txt");
+// 
+// 	GEngine->Run();
+// 
+// 	delete GEngine;
+// 
+// 	return 0;
+// }
