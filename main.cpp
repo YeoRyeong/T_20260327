5547d644183de4bdc479b2abb83c9c3d507e9731
Jun <tjrwns0012@naver.com>
2026-04-01 11:17:32 +0900

    윈도우 창에 색 넣기

--- main.cpp
+++ main.cpp
@@ -20,7 +20,11 @@ int SDL_main(int argc, char* argv[])
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
 
-	SDL_Window* MyWindow = SDL_CreateWindow("Hello", 100, 100, 640, 480, SDL_WINDOW_SHOWN); // 창 생성, title에 한글을 쓰려면 뭔가 추가해야함.
+	// 창 생성, title에 한글을 쓰려면 뭔가 추가해야함.-> 도화지
+	SDL_Window* MyWindow = SDL_CreateWindow("Hello", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	
+	// GPU, 화면을 그려주려면 붓이 필요함.
+	SDL_Renderer* MyRender = SDL_CreateRenderer(MyWindow, -1, 0);
 
 	while (true)
 	{
@@ -31,7 +35,15 @@ int SDL_main(int argc, char* argv[])
 		{
 			break;
 		}
+		// GPU한테 보낼 명령어모음
+		SDL_SetRenderDrawColor(MyRender, 192, 0, 16, 255);
+		SDL_RenderClear(MyRender); // 색을 지워야함.
+
+		SDL_RenderPresent(MyRender);
 	}
+	
+	SDL_DestroyRenderer(MyRender);
+
 	SDL_DestroyWindow(MyWindow);
 
 	SDL_Quit();
