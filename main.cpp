435af0bc9487bb2e1437cc640ebc18ba2a8c337a
Jun <tjrwns0012@naver.com>
2026-04-01 10:53:45 +0900

    창 열리는 이벤트

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 // lib 경로 추가
 // libaray 파일 등록
 
+
 // 사용할 라이브러리 파일 추가
 #pragma comment(lib, "SDL2")
 #pragma comment(lib, "SDL2main")
@@ -17,6 +18,23 @@ using namespace std;
 
 int SDL_main(int argc, char* argv[])
 {
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_Window* MyWindow = SDL_CreateWindow("Hello", 100, 100, 640, 480, SDL_WINDOW_SHOWN); // 창 생성, title에 한글을 쓰려면 뭔가 추가해야함.
+
+	while (true)
+	{
+		SDL_Event MyEvent;
+		SDL_PollEvent(&MyEvent); // 가져오기만 함.
+
+		if (MyEvent.type == SDL_QUIT)
+		{
+			break;
+		}
+	}
+	SDL_DestroyWindow(MyWindow);
+
+	SDL_Quit();
 
 	return 0;
 }
